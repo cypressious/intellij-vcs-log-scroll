15e64d1e3ee301c394423ace99cdd6763fe6ebbe
Kirill Rakhman <kirill.rakhman@gmail.com>
2024-10-15 16:31:32 +0200

    Implement action to scroll VCS log up

---
+++ src/main/kotlin/org/jetbrains/plugins/template/ScrollUpAction.kt
@@ -0,0 +1,19 @@
+package org.jetbrains.plugins.template
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.vcs.log.impl.VcsProjectLog
+import com.intellij.vcs.log.ui.VcsLogUiImpl
+import java.awt.Rectangle
+
+class ScrollUpAction : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        val project = event.project ?: return
+        val vcsProjectLog = VcsProjectLog.getInstance(project)
+        vcsProjectLog.logManager?.logUis?.forEach { ui ->
+            if (ui is VcsLogUiImpl) {
+                ui.table.scrollRectToVisible(Rectangle(0, 0));
+            }
+        }
+    }
+}
