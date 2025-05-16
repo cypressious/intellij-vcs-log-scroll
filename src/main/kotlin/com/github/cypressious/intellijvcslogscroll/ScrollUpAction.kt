3cf1d04fd0e65e0f2636fdabb2002bbeb56a50b4
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-05-16 19:35:42 +0200

    Move action to correct package

--- src/main/kotlin/com/github/cypressious/intellijvcslogscroll/ScrollUpAction.kt
+++ src/main/kotlin/com/github/cypressious/intellijvcslogscroll/ScrollUpAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cypressious.intellijvcslogscroll
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
