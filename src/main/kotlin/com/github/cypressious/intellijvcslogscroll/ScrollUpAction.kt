59d7f1e99b4e2ec26ccd6361d8c75fbc1d9b1c2f
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-05-16 19:49:19 +0200

    Take VCS UI from the context

--- src/main/kotlin/com/github/cypressious/intellijvcslogscroll/ScrollUpAction.kt
+++ src/main/kotlin/com/github/cypressious/intellijvcslogscroll/ScrollUpAction.kt
@@ -2,18 +2,15 @@ package com.github.cypressious.intellijvcslogscroll
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.vcs.log.impl.VcsProjectLog
+import com.intellij.vcs.log.VcsLogDataKeys.VCS_LOG_UI
 import com.intellij.vcs.log.ui.VcsLogUiImpl
 import java.awt.Rectangle
 
 class ScrollUpAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
-        val project = event.project ?: return
-        val vcsProjectLog = VcsProjectLog.getInstance(project)
-        vcsProjectLog.logManager?.logUis?.forEach { ui ->
-            if (ui is VcsLogUiImpl) {
-                ui.table.scrollRectToVisible(Rectangle(0, 0));
-            }
+        val ui = event.getData(VCS_LOG_UI)
+        if (ui is VcsLogUiImpl) {
+            ui.table.scrollRectToVisible(Rectangle(0, 0));
         }
     }
 }
